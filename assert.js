3d54ebe3bccb5710ed2ef0695a85e6613f1003eb
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-10-25 21:32:38 +0200

    Add assert

---
+++ assert.js
@@ -0,0 +1,47 @@
+const employees = [
+    {
+        name: 'Ali',
+        age: 25
+    },
+    {
+        name: 'Ahmed',
+        age: 27
+    }
+];
+
+const FIRST_INDEX = 0;
+
+const caseAssetFailedThatThrowErrorWithTraceAndData = () => {
+    console.assert(employees[FIRST_INDEX] === {
+        name: 'Ali',
+        age: 25
+    }, {
+        'employees[FIRST_INDEX]': employees[FIRST_INDEX],
+        'employee given': {
+            name: 'Ali',
+            age: 25
+        }
+    });
+}
+const isEmployeeMatchGivenInfo = (employee) => {
+    return employee.name === 'Ali' && employee.age === 25;
+};
+
+const caseAssetSuccessThatDoNoThing = () => {
+    const wantedEmployee = employees.filter(isEmployeeMatchGivenInfo)[FIRST_INDEX];
+    console.assert(employees[FIRST_INDEX] === wantedEmployee, {
+        'employees[FIRST_INDEX]': employees[FIRST_INDEX],
+        'wantedEmployee': wantedEmployee,
+        'message': 'Second'
+    });
+
+}
+
+
+caseAssetSuccessThatDoNoThing();
+caseAssetFailedThatThrowErrorWithTraceAndData();
+
+
+
+
+
